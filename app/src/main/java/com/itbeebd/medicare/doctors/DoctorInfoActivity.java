d3ae52118911c479d7871e8140b9767118275c31
Sumon-miazi <sumonmiazi59@gmail.com>
2020-10-13 13:50:02 +0600

    #Added empty activity named DoctorInfoActivity
    Signed-off-by: Sumon-miazi <sumonmiazi59@gmail.com>

---
+++ app/src/main/java/com/itbeebd/medicare/doctors/DoctorInfoActivity.java
@@ -0,0 +1,16 @@
+package com.itbeebd.medicare.doctors;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.itbeebd.medicare.R;
+
+public class DoctorInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_info);
+    }
+}
