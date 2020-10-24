fcdae5e4589d21fe31c7749c9e6a57d262b49d9a
Sumon-miazi <sumonmiazi59@gmail.com>
2020-10-24 14:25:19 +0600

    #Added empty UserSignInActivity and UserSignUpActivity.
    Signed-off-by: Sumon-miazi <sumonmiazi59@gmail.com>

---
+++ app/src/main/java/com/itbeebd/medicare/userProfile/UserSignInActivity.java
@@ -0,0 +1,16 @@
+package com.itbeebd.medicare.userProfile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.itbeebd.medicare.R;
+
+public class UserSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_sign_in);
+    }
+}
