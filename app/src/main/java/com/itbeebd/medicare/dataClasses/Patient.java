a63cf0ddbf58d4c757a75bfe7f6d078c27c7de4e
Sumon-miazi <sumonmiazi59@gmail.com>
2020-10-28 12:59:10 +0600

    #Added firebase dependencies.
    Signed-off-by: Sumon-miazi <sumonmiazi59@gmail.com>

--- app/src/main/java/com/itbeebd/medicare/dataClasses/Patient.java
+++ app/src/main/java/com/itbeebd/medicare/dataClasses/Patient.java
@@ -26,6 +26,9 @@ public class Patient implements Parcelable {
     private String phone;
     private String token;
 
+    public Patient() {
+    }
+
     public Patient(String name) {
         this.name = name;
         this.uid = "";
@@ -63,44 +66,83 @@ public class Patient implements Parcelable {
         parcel.writeString(this.token);
     }
 
-
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getUid() {
         return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 
     public String getGender() {
         return gender;
     }
 
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     public String getDob() {
         return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
     }
 
     public double getWeight() {
         return weight;
     }
 
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
     public String getBlood_group() {
         return blood_group;
+    }
+
+    public void setBlood_group(String blood_group) {
+        this.blood_group = blood_group;
     }
 
     public String getAddress() {
         return address;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public String getPhone() {
         return phone;
     }
 
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     public String getToken() {
         return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
