b20bf045db0121ba7db581980b74fb31d6774ee7
Sumon-miazi <sumonmiazi59@gmail.com>
2020-11-01 16:22:54 +0600

    #Added empty MedicationActivity
    Signed-off-by: Sumon-miazi <sumonmiazi59@gmail.com>

---
+++ app/src/main/java/com/itbeebd/medicare/medication/MedicationActivity.java
@@ -0,0 +1,16 @@
+package com.itbeebd.medicare.medication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.itbeebd.medicare.R;
+
+public class MedicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medication);
+    }
+}
